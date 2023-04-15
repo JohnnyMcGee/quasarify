1439377e0f512781c8b6bd4485c3d5d7d72e46f4
Johnny McGee <johnnymcgee.dev@gmail.com>
2023-04-15 01:32:06 -0500

    Add text transformer function

--- api.go
+++ api.go
@@ -8,14 +8,17 @@ import (
 )
 
 func init() {
-    // Register an HTTP function with the Functions Framework
-    functions.HTTP("api", myHTTPFunction)
+	// Register an HTTP function with the Functions Framework
+	functions.HTTP("api", myHTTPFunction)
 }
 
 // Function myHTTPFunction is an HTTP handler
 func myHTTPFunction(w http.ResponseWriter, r *http.Request) {
-    // Your code here
-
-    // Send an HTTP response
-    io.WriteString(w, "OK")
+	message := r.URL.Query().Get("message")
+	qMessage := ""
+	if message == "" {
+		qMessage = QuasarifyMessage(message)
+	}
+	// Send an HTTP response
+	io.WriteString(w, qMessage)
 }
---
+++ quasarify.go
@@ -0,0 +1,5 @@
+package api
+
+func QuasarifyMessage(message string) string {
+	return message + "... in space..."
+}
